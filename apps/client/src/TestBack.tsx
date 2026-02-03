f67f3725ac8361583ed01b129b9bdd7e827cd6f4
ffeaugas <feaugas.francis@gmail.com>
2026-02-03 13:49:47 +0100

    feat: monorepo setup

--- apps/client/src/TestBack.tsx
+++ apps/client/src/TestBack.tsx
@@ -1,22 +1,36 @@
 import { Button } from "@/components/ui/button";
+import { Input } from "./components/ui/input";
+import { api } from "@/lib/api";
 
 const TestBack = () => {
 
   const testBack = async () => {
-    try {
-      const res = await fetch("http://localhost:3001/");
-      const data = await res.text();
-      console.log(data);
-    } catch (error) {
-      console.error(error);
-    }
+    const { data, error } = await api.get()
+    console.log(data);
+  }
+
+  const handleSubmit = async (e: React.SubmitEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    const formData = new FormData(e.target as HTMLFormElement);
+    const id = formData.get("id") as string;
+
+    const { data, error } = await api.id({ id }).get({
+      query: { name: formData.get("name") as string }
+    })
+    console.log(error?.value.message);
+    console.log(data)
   }
 
   return (
     <div className="flex flex-col gap-6">
       <Button onClick={testBack}>test</Button>
+      <form onSubmit={handleSubmit}>
+        <Input type="text" placeholder="Enter id" name="id" />
+        <Input type="text" placeholder="Enter name" name="name" />
+        <Button type="submit">test</Button>
+      </form>
     </div>
   );
 }
 
-export default TestBack;
+export default TestBack;    
--- apps/server/src/index.ts
+++ apps/server/src/index.ts
@@ -1,11 +1,33 @@
 import { Elysia } from "elysia";
 import { cors } from "@elysiajs/cors";
+import { z } from "zod";
+
+const ipMiddleware = new Elysia()
+  .derive(
+    { as: 'global' }, ({ server, request }) => console.log(server?.requestIP(request))
+  )
 
 const app = new Elysia()
   .use(cors())
+  .use(ipMiddleware)
   .get("/", () => "Hello Elysia backend")
+  .get('/id/:id', ({ params: { id }, query: { name } }) => {
+    return {
+      id,
+      name
+    }
+  }, {
+    params: z.object({
+      id: z.coerce.number()
+    }),
+    query: z.object({
+      name: z.string().min(1, "Name is required"),
+    })
+  })
   .listen(3001);
 
 console.log(
   `🦊 Elysia is running at ${app.server?.hostname}:${app.server?.port}`
 );
+
+export type App = typeof app
